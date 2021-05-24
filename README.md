155910db796d231a92159841486e928ecf7eb436
Grant Matejka <grantmatejka1@gmail.com>
2021-05-24 16:27:21 -0700

    first major check in after finishing problem 1

--- README.md
+++ README.md
@@ -8,3 +8,20 @@ https://www.brinckerhoff.org/clements/2214-csc530/Assignments/rosette-123.html
 
 I'd like to just do #1 but probably will have to wait until monday to see what I can actually do
 
+# Major Update 1
+
+I think I got HW problem 1 completed. This is pretty cool bc I didn't know if I'd be able to do it.
+
+Learning rosette is very interesting and I think a major barrier for me was just really figuring out the difference between `solve` and `verify`. 
+
+I think the trouble in learning though is something with all tools as docs cannot be perfect and in a small community (i.e. rosette users) a google search hardly satisfies.
+
+Problem 1 took me much longer than I'd like to log haha.
+
+# How I did HW1
+
+I took the approach of applying solve first as this will tell me if a formula is a contradiction as it will return unsatisfiable (not be able to be solved)
+
+After we have categorized contradictions we can proceed to verify the formula and see if this returns unsatisfiable. Unsatisfiable in this setting means that no counterexample could be found and thus this formula would be a tautology but if there is a possible counterexample then this formula is indeed a contingency.
+
+
--- src/logic/classify.rkt
+++ src/logic/classify.rkt
@@ -7,26 +7,34 @@
 ; * 'CONTRADICTION if no interpretation I satisfies F;
 ; * 'CONTINGENCY if there are two interpretations I and I′ such that I satisfies F and I' does not.
 (define (classify F)
-  (match (solve F)
-    [(model stuff) stuff]
-    [(model) 'idk]
-    [(? unsat?) 'CONTRADICTION]))
+  (clear-vc!)
+  (match (solve (assert F))
+    ; if F is unsat then no I satisfies F
+    [(? unsat? f) 'CONTRADICTION]
+    ; if we get a model let's decide between tautology vs contingency
+    [(model found-solution)
+     ; if no solution verifies otherwise we have a tautology, else a contingency
+     (if (unsat? (verify (assert F))) 'TAUTOLOGY 'CONTIGENCY)]))
 
+
+; define our boolean variables of interest
 (define-symbolic* p q r boolean?)
 
-; (p → (q → r)) → (¬r → (¬q → ¬p))
+; (p → (q → r)) → (¬r → (¬q → ¬p)) CONTINGENCY
 (define f0 (=> (=> p (=> q r)) (=> (! r) (=> (! q) (! p)))))
 
-; (p ∧ q) → (p → q)
+; (p ∧ q) → (p → q) TAUTOLOGY
 (define f1 (=> (&& p q) (=> p q)))
 
-; (p ↔ q) ∧ (q → r) ∧ ¬(¬r → ¬p)
+; (p ↔ q) ∧ (q → r) ∧ ¬(¬r → ¬p) CONTRADICTION
 (define f2 (&& (<=> p q) (=> q r) (! (=> (! r) (! q)))))
 
+; p || -p TAUTOLOGY
+(define f3 (|| p (! p)))
 
 
-; TODO: Section 2.2 of rosette guide for how to do this stuff
+(classify f0)
+(classify f1)
+(classify f2)
+(classify f3)
 
-
-
-(solution? (solve f2))
